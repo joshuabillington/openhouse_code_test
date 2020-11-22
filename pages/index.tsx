58e5f8eb835aa65ffa0f350b9b7bf209aa0c7b58
joshuabillington <48261725+joshuabillington@users.noreply.github.com>
2020-11-22 08:42:56 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import DisplayCards from './components/DisplayCards'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 //using axios to fetch data from api 
